0a37f8e6b4b4293ea6fb74ad0ef28fe560c0d854
Ryan Sawadichai <ryan.sawadichai@outlook.com>
2023-07-13 13:35:48 -0600

    Initial Commit, database migration

---
+++ ShopOnline.Api/Entities/ProductCategory.cs
@@ -0,0 +1,8 @@
+namespace ShopOnline.Api.Entities;
+
+public class ProductCategory
+{
+    // Props named ID will be auto-designated as primary key
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
---
+++ ShopOnline.Api/Entities/User.cs
@@ -0,0 +1,7 @@
+namespace ShopOnline.Api.Entities;
+
+public class User
+{
+    public int Id { get; set; }
+    public string UserName { get; set; }
+}
