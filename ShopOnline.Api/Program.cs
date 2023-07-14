ea8c0c01d396ae9d357a853a14d19222839d06c0
ryans <raycosm@gmail.com>
2023-07-13 20:54:34 -0600

    Implemented API

--- ShopOnline.Api/Program.cs
+++ ShopOnline.Api/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using ShopOnline.Api.Data;
+using ShopOnline.Api.Repositories;
+using ShopOnline.Api.Repositories.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,9 +11,16 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 // Generate migration using EFCore. Use NuGet console `Add-Migration InitialCreate`
+// Also allows for dependency injection of ShopOnlineDbContext
 builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection")));
+
+// 
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
